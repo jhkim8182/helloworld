9407892ad39524fb6cb7994ab69a29c5c942802c
jhkim8182 <150390844+jhkim8182@users.noreply.github.com>
2023-11-11 13:04:51 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,3 +98,12 @@ option = st.selectbox(
     'What is your favorite color?',
     ('Blue', 'Red', 'Green'))
 st.write('Your favorite color is ', option)
+
+
+# Day 11
+st.header('st.multiselect')
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+st.write('You selected:', options)
