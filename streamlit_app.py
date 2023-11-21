da8846ba4609c4846a9232889ca644136c8ccca7
jhkim8182 <150390844+jhkim8182@users.noreply.github.com>
2023-11-20 21:14:05 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 from datetime import time, datetime
-from streamlit_pandas_profiling import st_profile_report
+
 
 st.header('st.button')
 
@@ -131,4 +131,4 @@ st.header('streamlit_pandas_profiling')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
 pr = df.profile_report()
-st_profile_report(pr)
+#st_profile_report(pr)
