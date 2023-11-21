8b795b654ed99b0653a8c388678338bc3407dc1b
jhkim8182 <150390844+jhkim8182@users.noreply.github.com>
2023-11-21 00:12:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,13 +124,3 @@ if coffee:
 if cola:
     st.write("Here you go :cola:")
 
-# Day 14
-import ydata_profiling
-from streamlit_pandas_profiling import st_profile_report
-
-st.header('streamlit_pandas_profiling')
-
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-
-pr = df.profile_report()
-st_profile_report(pr)
