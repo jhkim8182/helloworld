c8e82f1044c6c80c47cc99c5d24399f89170be06
jhkim8182 <150390844+jhkim8182@users.noreply.github.com>
2023-11-20 20:40:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ import numpy as np
 import altair as alt
 import pandas as pd
 from datetime import time, datetime
-#from streamlit_pandas_profiling import st_profile_report
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
 st.header('st.button')
 
