e11d3a762d87a40af875dc45deef2dfbfaf10ed5
jhkim8182 <150390844+jhkim8182@users.noreply.github.com>
2023-11-25 16:45:57 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,7 +128,7 @@ if cola:
 st.header('st.latex')
 
 st.latex(r''
-         a + ar + a r^2 +a r^3 + \cdots +a r^{n-1} =
+         a + a r + a r^2 +a r^3 + \cdots +a r^{n-1} =
          \sum_{k=0}^{n-1} ar^k =
          a \left(\frac{1-r^{n}}{1-r}\right}
          ''')
