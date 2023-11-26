b6cd36e6a50924a6df3746ff98d2082c8fd24608
jhkim8182 <150390844+jhkim8182@users.noreply.github.com>
2023-11-25 16:44:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,3 +124,11 @@ if coffee:
 if cola:
     st.write("Here you go :cola:")
 
+# Day 15
+st.header('st.latex')
+
+st.latex(r''
+         a + ar + a r^2 +a r^3 + \cdots +a r^{n-1} =
+         \sum_{k=0}^{n-1} ar^k =
+         a \left(\frac{1-r^{n}}{1-r}\right}
+         ''')
