c9f7717151f63842d999f40f15744391002c27e7
jhkim8182 <150390844+jhkim8182@users.noreply.github.com>
2023-11-29 13:39:54 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,3 +204,20 @@ else:
 #        st.write(f'🍴 **{user_food}** is your favorite **food**!')
 #    else:
 #        st.write('👈 Please choose your favorite **food**!')
+
+
+# Day 21
+import time
+
+st.title('st.progress')
+with st.expander('About this app'):
+     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.05)
+     my_bar.progress(percent_complete + 1)
+
+st.balloons()
+
